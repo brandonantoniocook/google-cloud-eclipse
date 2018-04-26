a0e2708e7232fd498d2d02957356b177403c111d
Brian de Alwis <bsd@mt.ca>
2018-04-25 23:55:40 -0400

    Better image handling

--- plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/ui/navigator/AppEngineLabelProvider.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/ui/navigator/AppEngineLabelProvider.java
@@ -1,11 +1,15 @@
-
 package com.google.cloud.tools.eclipse.appengine.facets.ui.navigator;
 
 import com.google.cloud.tools.appengine.AppEngineDescriptor;
+import com.google.cloud.tools.eclipse.appengine.ui.AppEngineImages;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 public class AppEngineLabelProvider extends LabelProvider {
+  private LocalResourceManager resources = new LocalResourceManager(JFaceResources.getResources());
+
   @Override
   public String getText(Object element) {
     if (element instanceof AppEngineDescriptor) {
@@ -18,6 +22,18 @@ public class AppEngineLabelProvider extends LabelProvider {
 
   @Override
   public Image getImage(Object element) {
+    if (element instanceof AppEngineDescriptor) {
+      return resources.createImage(AppEngineImages.appEngine(16));
+    }
     return null;
+  }
+
+  @Override
+  public void dispose() {
+    super.dispose();
+    if (resources != null) {
+      resources.dispose();
+      resources = null;
+    }
   }
 }
